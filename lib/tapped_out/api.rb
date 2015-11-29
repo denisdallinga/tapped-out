16876301d94c1818d5d1d16bbb66df19da360eee
DD Dallinga <denis.the.gatherer@gmail.com>
2015-11-30 00:04:14 +0100

    Update todo, module_function declaration consistent

--- lib/tapped_out/api.rb
+++ lib/tapped_out/api.rb
@@ -4,17 +4,16 @@ module TappedOut
     autoload :Base, 'tapped_out/api/base.rb'
     autoload :Deck, 'tapped_out/api/deck.rb'
 
+    module_function
+
     def featured_decks
     end
-    module_function :featured_decks
 
     def latest_decks
       Deck.latest
     end
-    module_function :latest_decks
 
     def deck_list
     end
-    module_function :deck_list
   end
 end
